c52efb7cbaaec2868e3f9d232cd1f35c9fa10029
Matan Radomski <MatanRad@users.noreply.github.com>
2024-06-21 17:02:04 +0300

    STT with wake word

---
+++ byte_fifo.py
@@ -0,0 +1,17 @@
+class ByteFIFO(object):
+    def __init__(self):
+        self._buf = bytearray()
+
+    def put(self, data):
+        self._buf.extend(data)
+
+    def get(self, size):
+        data = self._buf[:size]
+        self._buf[:size] = b''
+        return data
+
+    def peek(self, size):
+        return self._buf[:size]
+
+    def __len__(self):
+        return len(self._buf)
--- stt.py
+++ stt.py
@@ -1,4 +1,6 @@
 from google.cloud import speech
+import threading
+from byte_fifo import ByteFIFO
 
 def _requests_generator(stream):
     for chunk in stream:
@@ -21,3 +23,32 @@ class STT(object):
             for result in response.results:
                 if result.is_final:
                     return result.alternatives[0].transcript
+
+class STTAction(object):
+    def __init__(self, chunk_size=4096):
+        self._stt = STT()
+        self.result = None
+        self._fifo = ByteFIFO()
+        self._chunk_size = chunk_size
+        self._lock = threading.Lock()
+        self._thread = threading.Thread(target=self._run, args=(self._chunks_generator(),), daemon=True)
+        self._thread.start()
+
+    def handle_chunk(self, chunk):
+        with self._lock:
+            self._fifo.put(chunk)
+
+    def is_done(self) -> bool:
+        return not self._thread.is_alive()
+
+    def _chunks_generator(self):
+        while True:
+            chunk = None
+            with self._lock:
+                    chunk = self._fifo.get(self._chunk_size)
+            
+            if chunk is not None:
+                yield bytes(chunk)
+
+    def _run(self, generator) -> str:
+        self.result = self._stt.recognize_stream_command(generator)
